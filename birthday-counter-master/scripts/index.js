65edb48d11523274e4a9b4e5c58a332a6f919bee
Shreekumar hegde <43261685+shreekumarchittani@users.noreply.github.com>
2020-06-29 13:17:55 +0530

    Update index.js

--- birthday-counter-master/scripts/index.js
+++ birthday-counter-master/scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'June 29, 2020',
+  birthdate: 'July 1, 2020',
   name: 'Naagu'
 };
 
